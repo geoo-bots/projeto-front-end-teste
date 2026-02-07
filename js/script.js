c678c31332430d8ee12e6866244cf8f40d6ac04f
Geovana <dev.byul.c@gmail.com>
2026-02-07 04:55:28 -0300

    conteudo do projeto front end teste

---
+++ js/script.js
@@ -0,0 +1,17 @@
+function salvarUsuario(){
+    alert("Usuário adicionado!");
+}
+
+function salvarTarefa(){
+    alert("Tarefa adicionada!");
+}
+
+function salvarProjeto(){
+    alert("Projeto adicionado!");
+}
+
+
+function fecharFormulario(){
+    confirm('Tem certeza que deseja cancelar? Vou apagar tudão hein.');
+}
+
